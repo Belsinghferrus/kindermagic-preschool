f304f5a93cf6edd5d605822809b39b3c56778b42
apple <ferrusbelsingh@gmail.com>
2026-03-06 00:03:38 +0530

    home

--- js/index.js
+++ js/index.js
@@ -3,3 +3,28 @@ document.querySelectorAll('.program-card').forEach(card=>{
       card.querySelector('.program-inner').classList.toggle('rotate');
     });
   });
+
+
+
+  document.querySelectorAll(".faq-question").forEach(button => {
+
+    button.addEventListener("click", () => {
+    
+    const item = button.parentElement;
+    const answer = item.querySelector(".faq-answer");
+    const icon = button.querySelector(".faq-icon");
+    
+    document.querySelectorAll(".faq-answer").forEach(a => {
+    if(a !== answer) a.classList.add("hidden");
+    });
+    
+    document.querySelectorAll(".faq-icon").forEach(i => {
+    if(i !== icon) i.textContent = "+";
+    });
+    
+    answer.classList.toggle("hidden");
+    icon.textContent = answer.classList.contains("hidden") ? "+" : "−";
+    
+    });
+    
+    });
