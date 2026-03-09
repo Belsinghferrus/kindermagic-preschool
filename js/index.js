774247c8e32727dd9482f95a35da3de8e5466eef
apple <ferrusbelsingh@gmail.com>
2026-03-09 20:58:56 +0530

    200

--- js/index.js
+++ js/index.js
@@ -1,30 +1,42 @@
-document.querySelectorAll('.program-card').forEach(card=>{
-    card.addEventListener('click',()=>{
-      card.querySelector('.program-inner').classList.toggle('rotate');
+document.querySelectorAll('.program-card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.querySelector('.program-inner').classList.toggle('rotate');
     });
-  });
+});
 
 
 
-  document.querySelectorAll(".faq-question").forEach(button => {
+document.querySelectorAll(".faq-question").forEach(button => {
 
     button.addEventListener("click", () => {
-    
-    const item = button.parentElement;
-    const answer = item.querySelector(".faq-answer");
-    const icon = button.querySelector(".faq-icon");
-    
-    document.querySelectorAll(".faq-answer").forEach(a => {
-    if(a !== answer) a.classList.add("hidden");
+
+        const item = button.parentElement;
+        const answer = item.querySelector(".faq-answer");
+        const icon = button.querySelector(".faq-icon");
+
+        document.querySelectorAll(".faq-answer").forEach(a => {
+            if (a !== answer) a.classList.add("hidden");
+        });
+
+        document.querySelectorAll(".faq-icon").forEach(i => {
+            if (i !== icon) i.textContent = "+";
+        });
+
+        answer.classList.toggle("hidden");
+        icon.textContent = answer.classList.contains("hidden") ? "+" : "−";
+
     });
-    
-    document.querySelectorAll(".faq-icon").forEach(i => {
-    if(i !== icon) i.textContent = "+";
-    });
-    
-    answer.classList.toggle("hidden");
-    icon.textContent = answer.classList.contains("hidden") ? "+" : "−";
-    
-    });
-    
-    });
+
+});
+
+
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    menuIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+});
